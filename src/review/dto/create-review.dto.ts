54b674b28141a068da6a59a3795fc71187dcdb85
Alexandr <sashka.krup@gmail.com>
2021-09-02 21:35:50 +0300

    finish

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,10 +1,20 @@
-import { prop } from "@typegoose/typegoose"
-import { Types } from "mongoose"
+import { IsNumber, IsString, Max, Min } from "class-validator"
 
 export class CreateReviewDto {
+  @IsString()
   name: string
+
+  @IsString()
   title: string
+
+  @IsString()
   description: string
+
+  @Min(1)
+  @Max(5)
+  @IsNumber()
   rating: number
+
+  @IsString()
   productId: string
 }

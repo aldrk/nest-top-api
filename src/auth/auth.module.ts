ecd61c75b0ce2bd67c4975d6650a7f9b2aff521d
Alexandr <sashka.krup@gmail.com>
2021-09-02 11:24:08 +0300

    done review service and crud

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,8 +3,6 @@ import { AuthController } from "./auth.controller"
 import { TypegooseModule } from "nestjs-typegoose"
 import { AuthModel } from "./auth.model"
 
-console.log(AuthModel.prototype)
-
 @Module({
   controllers: [AuthController],
   imports: [
---
+++ src/review/review.constants.ts
@@ -0,0 +1,2 @@
+export const REVIEW_NOT_FOUND = "Review not found"
+export const REVIEWS_NOT_FOUND = "Reviews not found"

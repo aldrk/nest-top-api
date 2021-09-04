b1f9407c03b236dac2ca9e88f91a2f0467cf1eb3
Alexandr <sashka.krup@gmail.com>
2021-09-04 19:03:56 +0300

    add jwt guard, jwt decorator

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -7,12 +7,15 @@ import {
   HttpStatus,
   Param,
   Post,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from "@nestjs/common"
 import { CreateReviewDto } from "./dto/create-review.dto"
 import { ReviewService } from "./review.service"
 import { REVIEW_NOT_FOUND, REVIEWS_NOT_FOUND } from "./review.constants"
+import { JwtAuthGuard } from "../auth/guards/jwt.guard"
+import { UserEmail } from "../decorators/user-email.decorator"
 
 @Controller("review")
 export class ReviewController {
@@ -25,6 +28,7 @@ export class ReviewController {
     return this.reviewService.create(dto)
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(":id")
   async delete(@Param("id") id: string) {
     const deletedDoc = await this.reviewService.delete(id)
@@ -37,10 +41,12 @@ export class ReviewController {
   }
 
   @Get("byProduct/:productId")
-  async getByProduct(@Param("productId") productId: string) {
+  async getByProduct(@Param("productId") productId: string, @UserEmail() email: string) {
+    console.log(email)
     return this.reviewService.findByProductId(productId)
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete("byProduct/:productId")
   async deleteByProductId(@Param("productId") productId: string) {
     const deletedDocs = await this.reviewService.deleteByProductId(productId)

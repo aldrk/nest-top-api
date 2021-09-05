8d28950006a6f3547ab38bc81bab1f1ddf8f552d
Alexandr <sashka.krup@gmail.com>
2021-09-05 12:11:16 +0300

    product controller, product service, pipe for id validation

---
+++ src/pipes/id-validation.constants.ts
@@ -0,0 +1,1 @@
+export const INCORRECT_ID_ERROR = "Incorrect id format"
---
+++ src/pipes/id-validation.pipe.ts
@@ -0,0 +1,15 @@
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common"
+import { Types } from "mongoose"
+import { INCORRECT_ID_ERROR } from "./id-validation.constants"
+
+export class IdValidationPipe implements PipeTransform {
+  transform(value: string, metadata: ArgumentMetadata): any {
+    if (metadata.type !== "param") return value
+
+    if (!Types.ObjectId.isValid(value)) {
+      throw new BadRequestException(INCORRECT_ID_ERROR)
+    }
+
+    return value
+  }
+}

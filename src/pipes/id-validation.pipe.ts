0a34c07337da6e6d330b60dc197119d16c0faf59
Alexandr <sashka.krup@gmail.com>
2021-09-06 08:11:16 +0300

    add ts-ignore

--- src/pipes/id-validation.pipe.ts
+++ src/pipes/id-validation.pipe.ts
@@ -6,6 +6,7 @@ export class IdValidationPipe implements PipeTransform {
   transform(value: string, metadata: ArgumentMetadata): any {
     if (metadata.type !== "param") return value
 
+    // @ts-ignore
     if (!Types.ObjectId.isValid(value)) {
       throw new BadRequestException(INCORRECT_ID_ERROR)
     }
